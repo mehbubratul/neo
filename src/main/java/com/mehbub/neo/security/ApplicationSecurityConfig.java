e077b6e13f8da308b4e6f9d2fa526c6520888367
mehbubratul <ratulewu@gmail.com>
2022-05-30 17:36:01 +0600

    UserDetailsService and PasswordEncoder added

--- src/main/java/com/mehbub/neo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mehbub/neo/security/ApplicationSecurityConfig.java
@@ -1,9 +1,16 @@
 package com.mehbub.neo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /* Main class for spring security */
 
@@ -11,30 +18,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 
-    /*
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-            .authorizeRequests(
-                (authz) -> authz
-                    .antMatchers("index")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated()
-            )
-            .httpBasic();
-        http
-            .authorizeRequests()
-            .antMatchers("index", "/css/*, /js/*")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic();
-        return http.build();
-    }*/
+    private final PasswordEncoder passwordEncoder;
 
-   
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -46,5 +36,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
                 .and()
                 .httpBasic();
     }
+
+    /* 
+        purpose : How we are retrived users from db
+    */
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails polashUser =  User.builder()
+            .username("Polash")
+            .password( passwordEncoder.encode("password") )
+            .roles("Customer") //Spring will treat this as ROLE_Customer
+            .build();
+        return new InMemoryUserDetailsManager(polashUser);
+    }
+
+    
 
 }
